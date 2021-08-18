28b28c3a7c16e3eb6ee3a4a0480ff490c416c060
Kartikey Tewari <kartikeya30@gmail.com>
2021-08-19 01:10:39 +0530

    add main libraries

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Flower Class Prediction Algorithm
 ## This is a basic ML Project on the famous IRIS data set.
+
+Link to the tutorial is [here](https://analyticsindiamag.com/start-building-first-machine-learning-project-famous-dataset/)!
---
+++ predictor.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
